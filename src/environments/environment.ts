04f0c4c4dc3ad37c09cea43c559e11a8703998df
Avraam Nikolaou <avnikolaou@hotmail.com>
2022-04-29 11:41:52 +0300

    feat: firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAswZvOKHhpZ4StP4Yrvt8gA0jQgw68zAQ',
+    authDomain: 'clips-ce50f.firebaseapp.com',
+    projectId: 'clips-ce50f',
+    storageBucket: 'clips-ce50f.appspot.com',
+    appId: '1:1073888648114:web:4181f0daaf90f2173622ab',
+  },
 };
 
 /*
