967de1ed294d82016542c7d0a8eae155a021d8e9
Avraam Nikolaou <avnikolaou@hotmail.com>
2022-04-29 17:00:29 +0300

    feat: add signout functionality

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -10,12 +11,21 @@ import { AuthService } from '../services/auth.service';
 export class NavComponent implements OnInit {
   isAuthenticated = false;
 
-  constructor(public modal: ModalService, public auth: AuthService) {}
+  constructor(
+    public modal: ModalService,
+    public auth: AuthService,
+    private afAuth: AngularFireAuth
+  ) {}
 
   ngOnInit(): void {}
 
   openModal($event: Event) {
     $event.preventDefault();
     this.modal.toggleModal('auth');
+  }
+
+  async logout($event: Event) {
+    $event.preventDefault();
+    await this.afAuth.signOut();
   }
 }
