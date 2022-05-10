f7bb84f534fdc2e8f8405af05d85effaed1793fe
Avraam Nikolaou <avnikolaou@hotmail.com>
2022-05-10 16:17:04 +0300

    feat: add 'upload file' partial functionality

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.scss']
+  styleUrls: ['./upload.component.scss'],
 })
 export class UploadComponent implements OnInit {
+  isDragOver = false;
+  file: File | null = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  storeFile($event: Event) {
+    this.isDragOver = false;
+    this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null;
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+
+    console.log(this.file);
   }
-
 }
